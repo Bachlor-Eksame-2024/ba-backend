133a37b9973d1f11da428a07e203c3f27a09bdbe
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-14 10:29:13 +0100

    added Swagger ui

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const port = 3000;
+
+
+// Serve the Swagger docs at the `/api-docs` endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');

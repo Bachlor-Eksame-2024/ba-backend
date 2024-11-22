022ebd77c801e267356066c550b1a17d90d41d14
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-22 09:44:37 +0100

    more linting

--- server/models.py
+++ server/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime, timezone
 

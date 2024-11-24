a5145a802ca246d0837e9ef959ad432830bad26b
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-24 17:49:44 +0100

    added API KEY for Security

--- server/database.py
+++ server/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from fastapi import HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
 import os
 
 ## Postgres Database URL
@@ -27,3 +30,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+##### API ACCESS KEY #####
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = "X-API-Key"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API key"
+        )
